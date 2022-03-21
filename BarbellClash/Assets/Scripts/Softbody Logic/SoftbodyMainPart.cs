8b79991e10c441fd4d049fb4c778f30cec8c2cac
at-grigorievich <at.grigorievich@gmail.com>
2022-03-21 20:41:07 +0300

    Add main part crush logic

---
+++ BarbellClash/Assets/Scripts/Softbody Logic/SoftbodyMainPart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Barbell;
+using UnityEngine;
+
+namespace Softbody
+{
+    public class SoftbodyMainPart : MonoBehaviour
+    {
+        public event EventHandler OnCrushStart;
+        public event EventHandler OnCrushEnd;
+        
+        private IKinematic _kinematic;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(_kinematic != null || other.attachedRigidbody == null)
+                return;
+
+            if (other.attachedRigidbody.TryGetComponent(out IKinematic kinematic))
+            {
+                _kinematic = kinematic;
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if(_kinematic == null || other.attachedRigidbody == null)
+                return;
+
+            if (other.attachedRigidbody.TryGetComponent(out IKinematic kinematic))
+            {
+                if (ReferenceEquals(kinematic, _kinematic))
+                {
+                    _kinematic = null;
+                    OnCrushEnd?.Invoke(this,EventArgs.Empty);
+                }
+            }
+        }
+    }
+}
