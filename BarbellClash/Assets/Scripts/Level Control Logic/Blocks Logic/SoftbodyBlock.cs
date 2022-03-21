7657c505c97cee7ac06b76005862c1d5d9351e3b
at-grigorievich <at.grigorievich@gmail.com>
2022-03-21 20:42:20 +0300

    Add softbody detecting

--- BarbellClash/Assets/Scripts/Level Control Logic/Blocks Logic/SoftbodyBlock.cs
+++ BarbellClash/Assets/Scripts/Level Control Logic/Blocks Logic/SoftbodyBlock.cs
@@ -1,5 +1,6 @@
 using System;
 using ATG.LevelControl;
+using Barbell;
 using Softbody;
 using UnityEngine;
 using Zenject;
@@ -18,6 +19,8 @@ public class SoftbodyBlock : EnvironmentBlock
 
     [Space(15)] 
     [SerializeField] private BlockInstanceTarget _instanceOptions;
+
+    private IKinematic _curKinematic;
     
     public SoftbodyLogic Softbody { get; private set; }
     
@@ -27,5 +30,34 @@ public class SoftbodyBlock : EnvironmentBlock
         Softbody = GameObject.Instantiate(_softbodyInstance,transform);
         Softbody.transform.position = _instanceOptions.TargetPosition.position;
         Softbody.transform.rotation = _instanceOptions.TargetPosition.rotation;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(_curKinematic != null)
+            return;
+        
+        if (other.attachedRigidbody.TryGetComponent(out IKinematic k))
+        {
+            _curKinematic = k;
+            
+            Softbody.SetSoftbodyActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(_curKinematic == null)
+            return;
+
+        if (other.attachedRigidbody.TryGetComponent(out IKinematic k))
+        {
+            if (ReferenceEquals(k, _curKinematic))
+            {
+                _curKinematic = null;
+                
+                Softbody.SetSoftbodyActive(false);
+            }
+        }
     }
 }
--- BarbellClash/Assets/Scripts/Softbody Logic/SoftbodyLogic.cs
+++ BarbellClash/Assets/Scripts/Softbody Logic/SoftbodyLogic.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using Barbell;
 using UnityEngine;
 using Zenject;
 using Obi;
@@ -8,23 +9,34 @@ namespace Softbody
     [RequireComponent(typeof(ObiSolver),typeof(ObiFixedUpdater))]
     public abstract class SoftbodyLogic : MonoBehaviour
     {
+        [SerializeField] private SoftbodyMainPart _mainPart;
+        
         [SerializeField] private Vector3 _scale;
         
         protected ObiSolver _solver;
         protected ObiFixedUpdater _obiFixedUpdater;
-        
+
+        protected IKinematic _curKinematic;
+
         private IEnumerator Start()
         {
             _solver = GetComponent<ObiSolver>();
             _obiFixedUpdater = GetComponent<ObiFixedUpdater>();
+
+            _mainPart.OnCrushEnd +=
+                (sender, args) => _solver.parameters.damping = 1f;
             
             yield return new WaitForEndOfFrame();
             transform.localScale = _scale;
-
-            _solver.enabled = false;
-            _obiFixedUpdater.enabled = false;
+            SetSoftbodyActive(false);
         }
         
+
+        public void SetSoftbodyActive(bool isActive)
+        {
+            _solver.enabled = isActive;
+            _obiFixedUpdater.enabled = isActive;
+        }
         public class Factory: PlaceholderFactory<GameObject,SoftbodyLogic>{}
     }
 }
