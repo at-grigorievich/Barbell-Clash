738d95cdfe7c8b42c8090deb7e002f973651c5af
at-grigorievich <at.grigorievich@gmail.com>
2022-03-22 21:06:21 +0300

    Upd

---
+++ BarbellClash/Assets/Scripts/Softbody Logic/SoftbodyCrushVisualizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Obi;
+using UnityEngine;
+
+namespace Softbody
+{
+    public class SoftbodyCrushVisualizer : MonoBehaviour
+    {
+        [SerializeField] private ObiSoftbody _softbody;
+
+        private void Start()
+        {
+            var a = _softbody.softbodyBlueprint as ObiSoftbodySurfaceBlueprint;
+            Debug.Log(a.vertexToParticle.Length);
+        }
+    }
+}
