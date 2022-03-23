884381f153b823e05838509981bb5112baeab342
at-grigorievich <at.grigorievich@gmail.com>
2022-03-23 14:40:16 +0300

    Add main softbody colorize

--- BarbellClash/Assets/Scripts/Softbody Logic/SoftBodyCrushDetect.cs
+++ BarbellClash/Assets/Scripts/Softbody Logic/SoftBodyCrushDetect.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Obi;
+﻿using Obi;
 using UnityEngine;
 
 namespace Softbody
@@ -7,67 +6,39 @@ namespace Softbody
     [RequireComponent(typeof(SkinnedMeshRenderer),typeof(ObiSoftbody))]
     public class SoftBodyCrushDetect : MonoBehaviour
     {
+        [SerializeField] private float _limit;
+
         private SkinnedMeshRenderer _renderer;
-        private ObiSoftbody _currentSoftbody;
         private ObiSoftbodySkinner _skinner;
 
-        private Color[] _colors, _defaultColors;
+        private Color[] _defaultColors;
         
         private void Start()
         {
             _renderer = GetComponent<SkinnedMeshRenderer>();
-            _currentSoftbody = GetComponent<ObiSoftbody>();
             _skinner = GetComponent<ObiSoftbodySkinner>();
             
-            //_currentSoftbody.OnEndStep += CurrentSoftbodyOnOnEndStep;
-            //_currentSoftbody.solver.OnCollision += SolverOnOnCollision;
-
-            _colors = new Color[_renderer.sharedMesh.colors.Length];
             _defaultColors = (Color[])_renderer.sharedMesh.colors.Clone();
-            
-            Debug.Log(_skinner.m_softbodyInfluences.Length);
-            Debug.Log(_renderer.sharedMesh.colors.Length);
-            
-            for (var i = 0; i < _skinner.m_softbodyInfluences.Length; i++)
-            {
-                if (_skinner.m_softbodyInfluences[i] > 0.8f)
-                {
-                    _colors[i] = Color.green;
-                }
-            }
-
-            _renderer.sharedMesh.colors = _colors;
         }
 
         private void OnDisable()
         {
             _renderer.sharedMesh.colors = _defaultColors;
         }
-
-        private void CurrentSoftbodyOnOnEndStep(ObiActor actor, float steptime)
+        
+        public void ColorMainSoftbodyPart(Color color)
         {
-            //_renderer.sharedMesh.colors = _colors;
-        }
+            Color[] colors = (Color[]) _renderer.sharedMesh.colors.Clone();
 
-        private void SolverOnOnCollision(ObiSolver solver, ObiSolver.ObiCollisionEventArgs e)
-        {
-            foreach (Oni.Contact contact in e.contacts)
+            for (var i = 0; i < _skinner.m_softbodyInfluences.Length; i++)
             {
-                if (contact.distance < 0.01)
+                if (_skinner.m_softbodyInfluences[i] >= _limit)
                 {
-                    int particleIndex = _currentSoftbody.solverIndices[contact.bodyA];
-
-                    Vector3 position = _currentSoftbody.solver.positions[particleIndex];
-                    position = _currentSoftbody.GetParticlePosition(particleIndex);
-
-                    if (_skinner.m_softbodyInfluences[particleIndex] > 0.8f)
-                    {
-                        Debug.Log(_skinner.m_softbodyInfluences[particleIndex]);
-                    }
-                    //var b = new GameObject("t");
-                    //b.transform.position = position;
+                    colors[i] = color;
                 }
             }
+
+            _renderer.sharedMesh.colors = colors;
         }
     }
 }
