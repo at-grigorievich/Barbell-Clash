3bab7fec47a22edf94aabbd4288ed44e305212cf
at-grigorievich <at.grigorievich@gmail.com>
2022-03-24 16:48:01 +0300

    Add Tit girl

---
+++ BarbellClash/Assets/Scripts/Softbody Logic/Die Interactive Service/ShakeTitInteractable.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Softbody
+{
+    public class ShakeTitInteractable: DieInteractable
+    {
+        [SerializeField] private Transform _shakeTit;
+        
+        [SerializeField] private float _duration;
+        [SerializeField] private Vector3 _punchForce;
+        
+        [ContextMenu("Test Animation")]
+        public override void AnimateDie()
+        {
+            DOTween.Sequence()
+                .Append(_shakeTit.transform.DOPunchRotation(_punchForce, _duration))
+                .SetLoops(2,LoopType.Incremental);
+        }
+    }
+}
