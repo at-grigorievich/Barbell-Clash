1a47e5f085962339ff969fbe22ad8518a3c2b5fd
at-grigorievich <at.grigorievich@gmail.com>
2022-03-30 01:16:57 +0300

    Add obstacle blocks

---
+++ BarbellClash/Assets/Scripts/Obstacles/ObstacleBlock.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using ATG.LevelControl;
+using Barbell;
+using PlayerLogic;
+using UnityEngine;
+using VFXLogic;
+using Zenject;
+
+namespace Obstacles
+{
+    public class ObstacleBlock: EnvironmentBlock
+    {
+        [SerializeField] private ObstacleElement[] _obstacles;
+        [SerializeField] private Transform _hitEffectTarget;
+        
+        [Inject] private IVFXControllable _vfxControllable;
+        [Inject] private IBoostable _boostable;
+
+        private Action<BarbellLogic> _offObstacles;
+        
+        private void Awake()
+        {
+            _offObstacles = OffObstacles;
+        }
+
+        private void OffObstacles(BarbellLogic bl)
+        {
+            _offObstacles = null;
+
+            if (bl.HeightStatus == HeightStatus.Down)
+            {
+                _boostable.RemoveBoostSpeed();
+
+                var vfx = _vfxControllable.PlayVFX(VFXType.Death,
+                    _hitEffectTarget.position, 
+                    _hitEffectTarget.eulerAngles);
+                foreach (var obstacleElement in _obstacles)
+                {
+                    obstacleElement.OffObstacle();
+                }
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if(other.attachedRigidbody == null)
+                return;
+            
+            if(other.TryGetComponent(out IgnoreTriggers it))
+                return;
+
+            if (other.attachedRigidbody.TryGetComponent(out ICrushable crush))
+            {
+                if (crush is BarbellLogic bl)
+                {
+                    _offObstacles?.Invoke(bl);
+                }
+            }
+        }
+    }
+}
---
+++ BarbellClash/Assets/Scripts/Obstacles/ObstacleElement.cs
@@ -0,0 +1,16 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Obstacles
+{
+    public class ObstacleElement : MonoBehaviour
+    {
+        [SerializeField] private Vector3 _offVector;
+        [SerializeField] private float _rotateDuration;
+
+        public void OffObstacle()
+        {
+            transform.DORotate(_offVector, _rotateDuration);
+        } 
+    }
+}
